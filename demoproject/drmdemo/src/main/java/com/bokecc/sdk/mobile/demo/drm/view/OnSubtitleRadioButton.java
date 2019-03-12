e2534cbba4a6c2301f1b05969029d560469abb6f
Han Ruisong <550399108@qq.com>
2019-03-12 10:28:53 +0800

    升级到v2.19.0
    1. Demo新增播放加密音频功能；
    2. Demo新增由于断网导致的下载暂停，在恢复连接网络后自动启动下载；
    3. Demo修复m3u8格式的视频在断网后重连网络不能续播的问题；

---
+++ demoproject/drmdemo/src/main/java/com/bokecc/sdk/mobile/demo/drm/view/OnSubtitleRadioButton.java
@@ -0,0 +1,7 @@
+package com.bokecc.sdk.mobile.demo.drm.view;
+
+import android.widget.RadioButton;
+
+public interface OnSubtitleRadioButton {
+    void getRadioButtons(RadioButton fisrtSub, RadioButton secondSub, RadioButton twoSub);
+}
